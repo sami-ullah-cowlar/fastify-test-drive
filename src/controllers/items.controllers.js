c7e68e00cb11fb998b4db07b0040a3eb1a9c721f
sami-ullah-cowlar <sami.ullah.cowlar@gmail.com>
2022-10-30 03:26:41 +0500

    items crud complete

--- src/controllers/items.controllers.js
+++ src/controllers/items.controllers.js
@@ -1,21 +1,53 @@
-const items = require("../data/items");
+let items = require("../data/items");
 
 const getItems = (req, res) => {
-  console.log("items");
   const response = { success: true, data: items };
   res.send(response);
 };
 
 const getItem = (req, res) => {
   const { id } = req.params;
-  const response = {
-    success: true,
-    data: items.find((item) => item.id === Number(id)),
-  };
+  const item = items.find((item) => item.id === Number(id));
+  const response = { success: true, data: item };
+  res.send(response);
+};
+
+const createItem = (req, res) => {
+  const { name } = req.body;
+  const id = Math.floor(Math.random() * 1000);
+  const response = { success: true, data: { id, name } };
+  items = [...items, response.data];
+  res.status(201).send(response);
+};
+
+const updateItem = (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  let data;
+  items = items.map((item) => {
+    if (item.id === Number(id)) {
+      data = { ...item, name };
+      return data;
+    }
+    return item;
+  });
+
+  // items = items.filter((item) => item.id !== Number(id));
+  const response = { success: true, data };
+  res.send(response);
+};
+
+const deleteItem = (req, res) => {
+  const { id } = req.params;
+  items = items.filter((item) => item.id !== Number(id));
+  const response = { success: true, data: "Item Deleted Successfully" };
   res.send(response);
 };
 
 module.exports = {
   getItem,
   getItems,
+  createItem,
+  updateItem,
+  deleteItem,
 };
--- src/routes/items.routes.js
+++ src/routes/items.routes.js
@@ -1,4 +1,6 @@
-const { getItem, getItems } = require("../controllers/items.controllers");
+const { getItem, getItems, createItem, updateItem, deleteItem } = require(
+  "../controllers/items.controllers",
+);
 
 // Options for get all items
 const Item = {
@@ -42,9 +44,66 @@ const getItemOptions = {
   handler: getItem,
 };
 
+const createItemOptions = {
+  schema: {
+    body: {
+      type: "object",
+      required: ["name"],
+      properties: {
+        name: { type: "string" },
+      },
+    },
+    response: {
+      201: {
+        type: "object",
+        properties: {
+          success: { type: "boolean" },
+          data: Item,
+        },
+      },
+    },
+  },
+  handler: createItem,
+};
+
+const updateItemOptions = {
+  schema: {
+    response: {
+      200: {
+        type: "object",
+        properties: {
+          success: { type: "boolean" },
+          data: Item,
+        },
+      },
+    },
+  },
+  handler: updateItem,
+};
+
+const deleteItemOptions = {
+  schema: {
+    response: {
+      200: {
+        type: "object",
+        properties: {
+          success: { type: "boolean" },
+          data: {
+            type: "string",
+          },
+        },
+      },
+    },
+  },
+  handler: deleteItem,
+};
+
 function itemRoutes(app, options, done) {
   app.get("/items", getItemsOptions);
+  app.post("/items", createItemOptions);
   app.get("/items/:id", getItemOptions);
+  app.put("/items/:id", updateItemOptions);
+  app.delete("/items/:id", deleteItemOptions);
 
   done();
 }
